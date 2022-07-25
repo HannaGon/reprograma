0fc4c2d05938b11ceab7579b6faff9b8c8cb0d5c
Hanna <hanna.goncalves.pro@gmail.com>
2022-07-25 11:31:58 -0300

    feat: add date calc function

--- calculadora/js/desafio.js
+++ calculadora/js/desafio.js
@@ -3,13 +3,19 @@ console.log('LETS GOOOOOO')
 var valorHora = document.querySelector('#valor-hora')
 var horasProjeto = document.querySelector('#horas-projeto')
 var resultado = document.querySelector('#resposta')
+var dataPrazo = document.querySelector('#prazo-projeto')
+var dataAtual = Date.now()
+console.log ('data atual: '+dataAtual)
 
 function calcular(){
     var valor = valorHora.valueAsNumber
-    var horas = horasProjeto.valueAsNumber
+    var horas = horasProjeto.value
     var resposta = valor*horas
+    var dataPrazoMl = Date.parse(dataPrazo.value)
+    console.log('prazo: '+dataPrazoMl)
     console.log(resposta)
-    if (true) {
+    
+    if (dataPrazoMl-dataAtual>604800000) {
         resultado.textContent = 'R$ ' +resposta+',00'
     }
     else  {
